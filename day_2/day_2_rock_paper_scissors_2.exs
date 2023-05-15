9fd69e728b3dd2d516ae08d9f4ecec3102807569
FriendlyPigeon <levi.liukkonen@gmail.com>
2023-05-14 22:41:44 -0700

    Add day 2 part 2

---
+++ day_2/day_2_rock_paper_scissors_2.exs
@@ -0,0 +1,27 @@
+case File.open("input.txt", [:read]) do
+  {:ok, file} ->
+    total_score = Enum.reduce(IO.stream(file, :line), 0, fn line, acc ->
+      enemy_hand = String.at(line, 0)
+      outcome = String.at(line, 2)
+
+      # compute score for player choice
+      outcome_score = case outcome do
+        "X" -> 0
+        "Y" -> 3
+        "Z" -> 6
+      end
+
+      # compute score for outcome
+      choice_score = case {enemy_hand, outcome} do
+        {a, "X"} -> a == "A" && 3 || a == "B" && 1 || a == "C" && 2
+        {a, "Y"} -> a == "A" && 1 || a == "B" && 2 || a == "C" && 3
+        {a, "Z"} -> a == "A" && 2 || a == "B" && 3 || a == "C" && 1
+      end
+
+      acc + choice_score + outcome_score
+    end)
+
+    IO.puts "Total score: #{total_score}"
+  {:error, reason} ->
+    IO.puts "Error opening input file: #{reason}"
+end
